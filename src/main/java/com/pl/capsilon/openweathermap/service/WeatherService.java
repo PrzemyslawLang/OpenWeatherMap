11139b4c25c977a3e5ed953dcfc1998ab563207e
Przemyslaw <anonim@anonim.pl>
2018-05-15 22:49:04 +0200

    Fix some errors

--- src/main/java/com/pl/capsilon/openweathermap/service/WeatherService.java
+++ src/main/java/com/pl/capsilon/openweathermap/service/WeatherService.java
@@ -1,6 +1,7 @@
 package com.pl.capsilon.openweathermap.service;
 
 import com.pl.capsilon.openweathermap.dto.OpenWeatherMapResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -8,8 +9,13 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherService {
 
+    private RestTemplate restTemplate;
+
+    public WeatherService(@Autowired RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public OpenWeatherMapResponseDto getWeather(String city, String countryCode){
-        RestTemplate restTemplate = new RestTemplate();
         String url = String.format("http://samples.openweathermap.org/data/2.5/forecast?q=%s,%s&appid=b6907d289e10d714a6e88b30761fae22", city, countryCode);
         try {
             return restTemplate.getForObject(url, OpenWeatherMapResponseDto.class);
--- src/main/java/com/pl/capsilon/openweathermap/utility/Calculation.java
+++ src/main/java/com/pl/capsilon/openweathermap/utility/Calculation.java
@@ -9,23 +9,18 @@ public class Calculation {
 
     static public WeatherDto getAverages(List<SingleMeasurementDto> list){
         WeatherDto weatherDto = new WeatherDto();
-        weatherDto.setAverageHumidity(Math.round(getAverageHumidity(list)*100.0)/100.0);
-        weatherDto.setAveragePressure(Math.round(getAveragePressure(list)*100.0)/100.0);
-        weatherDto.setAverageTemperature(Math.round(getAverageTemperature(list)*100.0)/100.0);
+        Double humidity = 0D;
+        Double pressure = 0D;
+        Double temperature = 0D;
+        for(SingleMeasurementDto measurementDto : list){
+            humidity += measurementDto.getMain().getHumidity();
+            pressure += measurementDto.getMain().getPressure();
+            temperature += (measurementDto.getMain().getTemp_max()+measurementDto.getMain().getTemp_min())/2;
+        }
+
+        weatherDto.setAverageHumidity(Math.round((humidity/list.size())*100.0)/100.0);
+        weatherDto.setAveragePressure(Math.round((pressure/list.size())*100.0)/100.0);
+        weatherDto.setAverageTemperature(Math.round((temperature/list.size())*100.0)/100.0);
         return weatherDto;
-    }
-
-    static private Double getAverageHumidity(List<SingleMeasurementDto> list){
-        return list.stream().mapToDouble(singleMeasurement -> singleMeasurement.getMain().getHumidity()).average().orElse(0);
-    }
-
-    static private Double getAveragePressure(List<SingleMeasurementDto> list){
-        return list.stream().mapToDouble(singleMeasurement -> singleMeasurement.getMain().getPressure()).average().orElse(0);
-    }
-
-    static private Double getAverageTemperature(List<SingleMeasurementDto> list){
-        Double maxTemp = list.stream().mapToDouble(singleMeasurement -> singleMeasurement.getMain().getTemp_max()).average().orElse(0);
-        Double minTemp = list.stream().mapToDouble(singleMeasurement -> singleMeasurement.getMain().getTemp_min()).average().orElse(0);
-        return (maxTemp+minTemp)/2;
     }
 }
